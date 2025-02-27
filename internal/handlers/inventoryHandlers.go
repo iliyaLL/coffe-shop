3d4a1b23e1499d07f57223c64674077d67ca2b9d
iliya <limilia248@gmail.com>
2025-02-27 13:14:03 +0500

    feat:inventoryInsert

--- internal/handlers/inventoryHandlers.go
+++ internal/handlers/inventoryHandlers.go
@@ -1,10 +1,39 @@
 package handlers
 
 import (
+	"encoding/json"
+	"errors"
+	"frappuccino/internal/models"
+	"frappuccino/internal/utils"
 	"net/http"
 )
 
 func (app *application) inventoryCreatePost(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hello world"))
+	w.Header().Set("Content-Type", "application/json")
+
+	var inventory models.Inventory
+	err := json.NewDecoder(r.Body).Decode(&inventory)
+	if err != nil {
+		utils.SendJSONResponse(w, http.StatusBadRequest, map[string]string{"error": "request body does not match json format"})
+		return
+	}
+	defer r.Body.Close()
+
+	m, err := app.InventorySvc.Insert(&inventory)
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateInventory) {
+			utils.SendJSONResponse(w, http.StatusBadRequest, map[string]string{"error": "duplicate inventory"})
+		} else if errors.Is(err, models.ErrNegativeQuantity) {
+			utils.SendJSONResponse(w, http.StatusBadRequest, map[string]string{"error": "negative quantity"})
+		} else if errors.Is(err, models.ErrMissingFields) {
+			utils.SendJSONResponse(w, http.StatusBadRequest, m)
+		} else if errors.Is(err, models.ErrInvalidEnumType) {
+			utils.SendJSONResponse(w, http.StatusBadRequest, map[string]string{"error": "invalid unit type", "supported types": "shots, ml, g, units"})
+		} else {
+			utils.SendJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": "internal server error"})
+		}
+		return
+	}
+
+	utils.SendJSONResponse(w, http.StatusCreated, map[string]string{"message": "created"})
 }
--- internal/models/error.go
+++ internal/models/error.go
@@ -3,8 +3,9 @@ package models
 import "errors"
 
 var (
-	ErrDuplicateInventory = errors.New("models: duplicate inventory")
 	ErrNoRecord           = errors.New("models: no record")
+	ErrDuplicateInventory = errors.New("models: duplicate inventory")
 	ErrNegativeQuantity   = errors.New("models: positive_quantity constraint violation")
 	ErrMissingFields      = errors.New("models: missing fields")
+	ErrInvalidEnumType    = errors.New("models: invalid enum type")
 )
