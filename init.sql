59d38af1e9d3882d8150dab501b052623ec1ab96
iliya <limilia248@gmail.com>
2025-03-20 12:31:07 +0500

    add triggers to track changes in orders, menu_items, and inventory

--- init.sql
+++ init.sql
@@ -79,3 +79,59 @@ INSERT INTO inventory (name, quantity, unit, categories) VALUES
 ('Pastry Dough', 5000, 'g', ARRAY['Baking']),
 ('Butter', 2000, 'g', ARRAY['Dairy']),
 ('Eggs', 300, 'units', ARRAY['Baking', 'Dairy']);
+
+-- Function for inventory quantity tracking
+CREATE OR REPLACE FUNCTION log_inventory_transaction()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF OLD.quantity <> NEW.quantity THEN
+        INSERT INTO inventory_transactions (inventory_id, old_quantity, new_quantity, transaction_date)
+        VALUES (NEW.id, OLD.quantity, NEW.quantity, NOW());
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Trigger to track quantity updates in inventory
+CREATE TRIGGER after_inventory_update
+AFTER UPDATE ON inventory
+FOR EACH ROW
+EXECUTE FUNCTION log_inventory_transaction();
+
+
+-- Function for price change tracking
+CREATE OR REPLACE FUNCTION log_price_change()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF OLD.price <> NEW.price THEN
+        INSERT INTO price_history (menu_item_id, old_price, new_price, updated_at)
+        VALUES (NEW.id, OLD.price, NEW.price, NOW());
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Trigger to track price updates in menu_items
+CREATE TRIGGER after_price_update
+AFTER UPDATE ON menu_items
+FOR EACH ROW
+EXECUTE FUNCTION log_price_change();
+
+
+-- Function for order status tracking
+CREATE OR REPLACE FUNCTION log_order_status_change()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF OLD.order_status <> NEW.order_status THEN
+        INSERT INTO order_status_history (order_id, updated_at, old_status, new_status)
+        VALUES (NEW.id, NOW(), OLD.order_status, NEW.order_status);
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Trigger to track status updates in orders
+CREATE TRIGGER after_order_status_update
+AFTER UPDATE ON orders
+FOR EACH ROW
+EXECUTE FUNCTION log_order_status_change();
